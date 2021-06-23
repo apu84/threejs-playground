33a2c8ab41df58c8d60a9e9071d9583db6d4ec13
Moshlehuddin <apu@stibodx.com>
2021-06-23 22:24:50 +0600

    Added css loader, style loader and less loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,49 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: {
-    main: path.resolve(__dirname, './src/index.js')
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js'
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
-      filename: "index.html"
-    }),
-    new CleanWebpackPlugin()
-  ],
-
-  module: {
-    rules: [{
-     test: /\.js$/,
-      exclude: /node_modules/,
-      use: ['babel-loader']
+module.exports = env => {
+  return {
+    mode: env.mode || 'none',
+    entry: {
+      main: path.resolve(__dirname, './src/index.js')
     },
-    ]
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: '[name].bundle.js'
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './src/index.html'),
+        filename: "index.html"
+      }),
+      new CleanWebpackPlugin()
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+          type: 'asset/inline',
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.less/,
+          use: ['style-loader', 'css-loader', 'less-loader']
+        }
+      ]
+    }
   }
 }
