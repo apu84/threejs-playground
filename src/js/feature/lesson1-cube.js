5aaccc3998c0b2adcb4345fbaba2e1293c086da3
Moshlehuddin <apu@stibodx.com>
2021-06-24 11:03:48 +0600

    - Added a color for the cube. Mouse click will change the cube color randomly.

--- src/js/feature/lesson1-cube.js
+++ src/js/feature/lesson1-cube.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 
-function lesson1rotatingCube () {
+function lesson1rotatingCube() {
   const scene = new THREE.Scene();
   let fov = 75;
   const camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, .1, 1000);
@@ -24,14 +24,27 @@ function lesson1rotatingCube () {
 
   animate();
 
+  const colors = ['red', 'purple', 'green', 'yellow', 'white', 'teal', 'blue', 'brown', 'chocolate', 'coral', 'burlywood'];
+
+  function getColor() {
+    return colors[getRandom(colors.length)];
+  }
+
+  function getRandom(max, min = 0) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  document.addEventListener('click', (event) => {
+    cube.material = new THREE.MeshBasicMaterial({ color: getColor() });
+  });
+
   document.addEventListener('wheel', (event) => {
     if (event.deltaY >= 0) {
-      if(fov + 1 <= 180) {
+      if (fov + 1 <= 180) {
         fov += 1
       }
-    }
-    else {
-      if(fov + 1 >= 0) {
+    } else {
+      if (fov + 1 >= 0) {
         fov -= 1;
       }
     }
